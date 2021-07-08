a3c612c5abb49315a2fe8c27f955e4adeb6effb4
Carl Summers <31711543+CSumm@users.noreply.github.com>
2021-07-08 15:16:53 -0400

    Fix open graph data

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "title": "Web Starter",
-  "description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
-  "url": "https://webstarter.chriscollins.me",
-  "author": "Chris Collins",
+  "title": "Stocker",
+  "description": "Follow the stocks you invest in without cost or subscription",
+  "url": "https://carldesigns.online/",
+  "author": "Carl Summers",
   "meta_data": {
-    "twitter": "@scottishstoater",
+    "twitter": "@cjksummers",
     "default_social_image": "/static/default_social_image.jpg"
   },
   "env": process.env.ELEVENTY_ENV === 'production'
